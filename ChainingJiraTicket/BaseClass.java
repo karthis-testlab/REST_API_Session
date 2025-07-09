b7b9da7aa835652576d9c1d223f9e840c03fcf93
Karthikeyan Rajendran <Karthikeyan_Rajendran@epam.com>
2025-07-09 09:13:29 +0200

    fix code

--- ChainingJiraTicket/BaseClass.java
+++ ChainingJiraTicket/BaseClass.java
@@ -17,6 +17,6 @@ public class BaseClass {
 		
 		//Jira Authentication
 		
-		RestAssured.authentication=RestAssured.preemptive().basic("Feb2023restAPI@gmail.com", "ATATT3xFfGF07l3P383cEkq9UxdU7W6WPYnDbilxk0f-c8JT1LB9ubgHUyJz3qa2NtLfldtwYIPCJhXlgAsYSMIaScp5YfEELQyTa0EJ8PoA8I8h8pVBZqCM7v1zTgxQ87vIT6RyqL4v5y1KChu-kuE1hDOdKIeuWi1nyqG4OVf--_ph9sA7b58=0DFB5058");
+		RestAssured.authentication=RestAssured.preemptive().basic("Feb2023restAPI@gmail.com", "");
 	}
 }
